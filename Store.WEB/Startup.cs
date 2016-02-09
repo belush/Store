1d4202eaf52c6df56183d3000455dbd191b899cf
zheka <belushko@gmail.com>
2016-02-09 22:41:45 +0200

    create project

---
+++ Store.WEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Store.WEB.Startup))]
+namespace Store.WEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
